73751607b7873fbef919e655e835c3ed6a425e73
HP 250g7 <brightology2@gmain.com>
2023-05-16 17:37:06 +0100

    Expense Tracker

---
+++ src/main/java/com/brightology/expensetracker/config/DatabaseChangeLog.java
@@ -0,0 +1,11 @@
+package com.brightology.expensetracker.config;
+
+//@ChangeLog
+public class DatabaseChangeLog {
+
+   // @ChangeSet(order = "001", id = "seedDatabase", author = "Sai")
+    public void setDatabase(){
+
+
+    }
+}
---
+++ src/main/java/com/brightology/expensetracker/model/ExpenseCategory.java
@@ -0,0 +1,5 @@
+package com.brightology.expensetracker.model;
+
+public enum ExpenseCategory {
+    ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+}
